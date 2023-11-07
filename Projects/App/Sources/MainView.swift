70478a3fa98d696f7395542ff6cbc5ceac3efbbb
Hajime <impapa68@gmail.com>
2023-11-07 15:38:55 +0900

    add: MainView 파일 추가

--- Projects/App/Sources/MainView.swift
+++ Projects/App/Sources/MainView.swift
@@ -1,19 +1,18 @@
 //
-//  ContentView.swift
+//  MainView.swift
 //  App
 //
 //  Created by Ha Jong Myeong on 11/7/23.
 //
 
 import SwiftUI
-import AVFoundation
 
-struct ContentView: View {
+struct MainView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ContentView()
+    MainView()
 }
--- Projects/App/Sources/MyApp.swift
+++ Projects/App/Sources/MyApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MyApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
 }
