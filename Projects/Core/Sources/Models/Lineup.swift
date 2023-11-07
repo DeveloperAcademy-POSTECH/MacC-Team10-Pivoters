21a7ea4299c755228c9450e3eb8ca5a4865830e1
sozohoy <sozohoy@gmail.com>
2023-11-07 15:02:21 +0900

    feat: Lineup 모델 구현(#3)

--- Projects/Core/Sources/Models/Lineup.swift
+++ Projects/Core/Sources/Models/Lineup.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+public final class Lineup {
+    public let id: UUID
+    public let uniform: Uniform
+    public let headcount: Headcount
+    public let players: [Player]
+
+    public init(
+        id: UUID,
+        uniform: Uniform,
+        headcount: Headcount,
+        players: [Player]) {
+            self.id = id
+            self.uniform = uniform
+            self.headcount = headcount
+            self.players = players
+        }
+}
+
+public enum Uniform: Codable {
+    case basic
+}
+
+public enum Headcount: Int, Codable {
+    case five = 5
+    case six = 6
+    case nine = 9
+    case eleven = 11
+}
