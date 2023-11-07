1d2f7067bd9eefe85d68b76810c82d9f5ab1b1dd
sozohoy <sozohoy@gmail.com>
2023-11-07 17:21:45 +0900

    add: UniformView(#11)

---
+++ Projects/Feature/Sources/UniformView.swift
@@ -0,0 +1,19 @@
+//
+//  UniformView.swift
+//  Feature
+//
+//  Created by 한지석 on 11/7/23.
+//  Copyright © 2023 com.pivoters. All rights reserved.
+//
+
+import SwiftUI
+
+struct UniformView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UniformView()
+}
