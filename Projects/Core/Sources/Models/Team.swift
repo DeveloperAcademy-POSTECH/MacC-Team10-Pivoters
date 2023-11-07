03c30d33d0d2525bfee69b10bad9b8d85136f6b5
sozohoy <sozohoy@gmail.com>
2023-11-07 15:02:11 +0900

    feat: Team 모델 구현(#3)

--- Projects/Core/Sources/Models/Team.swift
+++ Projects/Core/Sources/Models/Team.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+public final class Team {
+    public let id: UUID
+    public let teamName: String
+    public let subTeamName: String
+    public let lineup: [Lineup]
+
+    public init(
+        id: UUID,
+        teamName: String,
+        subTeamName: String,
+        lineup: [Lineup]) {
+            self.id = id
+            self.teamName = teamName
+            self.subTeamName = subTeamName
+            self.lineup = lineup
+        }
+}
+
