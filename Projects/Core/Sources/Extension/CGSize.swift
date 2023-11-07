c617f2fddcb5a015d19127b97c4e55a824cfeda5
eemdeeks <prince9088@gmail.com>
2023-11-07 21:36:25 +0900

    fix: 제약조건 수정(#13)

--- Projects/Core/Sources/Extension/CGSize.swift
+++ Projects/Core/Sources/Extension/CGSize.swift
@@ -11,13 +11,13 @@ import Foundation
 extension CGSize {
     public static func + (lhs: Self, rhs: Self) -> Self {
         if lhs.width + rhs.width > 0, lhs.height + rhs.height > 0 {
-            CGSize(width: min(lhs.width + rhs.width, 200), height: min(lhs.height + rhs.height, 150))
+            CGSize(width: min(lhs.width + rhs.width, 180), height: min(lhs.height + rhs.height, 120))
         } else if lhs.width + rhs.width < 0, lhs.height + rhs.height < 0 {
-            CGSize(width: max(lhs.width + rhs.width, -200), height: max(lhs.height + rhs.height, -150))
+            CGSize(width: max(lhs.width + rhs.width, -180), height: max(lhs.height + rhs.height, -110))
         } else if lhs.width + rhs.width > 0, lhs.height + rhs.height < 0 {
-            CGSize(width: min(lhs.width + rhs.width, 200), height: max(lhs.height + rhs.height, -150))
+            CGSize(width: min(lhs.width + rhs.width, 180), height: max(lhs.height + rhs.height, -110))
         } else if lhs.width + rhs.width < 0, lhs.height + rhs.height > 0 {
-            CGSize(width: max(lhs.width + rhs.width, -200), height: min(lhs.height + rhs.height, 150))
+            CGSize(width: max(lhs.width + rhs.width, -180), height: min(lhs.height + rhs.height, 120))
         } else {
             CGSize(width: lhs.width + rhs.width, height: lhs.height + rhs.height)
         }
