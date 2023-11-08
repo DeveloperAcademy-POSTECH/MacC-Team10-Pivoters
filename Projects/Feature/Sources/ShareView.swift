48e3ca21303f22b3d85369013effea5777d28c7d
Hajime <impapa68@gmail.com>
2023-11-08 12:27:02 +0900

    add: ShareView 추가(#15)

---
+++ Projects/Feature/Sources/ShareView.swift
@@ -0,0 +1,22 @@
+//
+//  ShareView.swift
+//  Feature
+//
+//  Created by Ha Jong Myeong on 11/8/23.
+//  Copyright © 2023 com.pivoters. All rights reserved.
+//
+
+import SwiftUI
+
+public struct ShareView: View {
+
+    public init() {}
+    
+    public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShareView()
+}
