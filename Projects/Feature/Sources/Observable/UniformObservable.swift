1a5a5ce5b68007bf5c8811444c1a503eb35076c4
sozohoy <sozohoy@gmail.com>
2023-11-08 14:44:44 +0900

    feat: Uniform Observable 프로퍼티 생성, 목데이터 생성(#11)

--- Projects/Feature/Sources/Observable/UniformObservable.swift
+++ Projects/Feature/Sources/Observable/UniformObservable.swift
@@ -7,3 +7,75 @@
 //
 
 import Foundation
+import SwiftUI
+
+import Core
+
+@Observable
+final class UniformObservable {
+    var lineup = Lineup(id: UUID(),
+                        uniform: .basic,
+                        headcount: .eleven,
+                        players: MockData.player,
+                        primaryColor: .black)
+}
+
+struct MockData {
+    static let player: [Player] = [
+        Player(name: "호날두",
+               number: 1,
+               isGoalkeeper: true,
+               offset: OffsetValue(draggedOffset: CGSize(width: 0, height: 100),
+                                   accumulatedOffset: CGSize(width: 0, height: 100))),
+        Player(name: "호날두",
+               number: 2,
+               isGoalkeeper: true,
+               offset: OffsetValue(draggedOffset: CGSize(width: -75, height: 50),
+                                   accumulatedOffset: CGSize(width: -75, height: 50))),
+        Player(name: "호날두",
+               number: 3,
+               isGoalkeeper: true,
+               offset: OffsetValue(draggedOffset: CGSize(width: 75, height: 50),
+                                   accumulatedOffset: CGSize(width: 75, height: 50))),
+        Player(name: "호날두",
+               number: 4,
+               isGoalkeeper: true,
+               offset: OffsetValue(draggedOffset: CGSize(width: 140, height: 30),
+                                   accumulatedOffset: CGSize(width: 140, height: 30))),
+        Player(name: "호날두",
+               number: 5,
+               isGoalkeeper: true,
+               offset: OffsetValue(draggedOffset: CGSize(width: -140, height: 30),
+                                   accumulatedOffset: CGSize(width: -140, height: 30))),
+        Player(name: "호날두",
+               number: 6,
+               isGoalkeeper: true,
+               offset: OffsetValue(draggedOffset: CGSize(width: 0, height: 20),
+                                   accumulatedOffset: CGSize(width: 0, height: 20))),
+        Player(name: "호날두",
+               number: 7,
+               isGoalkeeper: true,
+               offset: OffsetValue(draggedOffset: CGSize(width: 70, height: -20),
+                                   accumulatedOffset: CGSize(width: 70, height: -20))),
+        Player(name: "호날두",
+               number: 8,
+               isGoalkeeper: true,
+               offset: OffsetValue(draggedOffset: CGSize(width: -70, height: -20),
+                                   accumulatedOffset: CGSize(width: -70, height: -20))),
+        Player(name: "호날두",
+               number: 9,
+               isGoalkeeper: true,
+               offset: OffsetValue(draggedOffset: CGSize(width: 0, height: -80),
+                                   accumulatedOffset: CGSize(width: 0, height: -80))),
+        Player(name: "호날두",
+               number: 10,
+               isGoalkeeper: true,
+               offset: OffsetValue(draggedOffset: CGSize(width: 130, height: -80),
+                                   accumulatedOffset: CGSize(width: 130, height: -80))),
+        Player(name: "호날두",
+               number: 11,
+               isGoalkeeper: true,
+               offset: OffsetValue(draggedOffset: CGSize(width: -130, height: -80),
+                                   accumulatedOffset: CGSize(width: -130, height: -80)))
+    ]
+}
