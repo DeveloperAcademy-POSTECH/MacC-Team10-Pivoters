ed44950ae12299db9a0e7148f2ec7837970c4f6b
Hajime <impapa68@gmail.com>
2023-11-09 02:40:23 +0900

    feat: ImageMetaDataProvider 추가

--- Projects/Feature/Sources/ShareView.swift
+++ Projects/Feature/Sources/ShareView.swift
@@ -6,7 +6,9 @@
 //  Copyright © 2023 com.pivoters. All rights reserved.
 //
 
+import Foundation
 import SwiftUI
+import LinkPresentation
 
 public struct ShareView: View {
 
@@ -23,4 +25,40 @@ struct ShareImage: View {
         Color.red
             .frame(width: 200, height: 200)
     }
+}
+
+class ImageMetadataProvider: UIActivityItemProvider {
+  var image: UIImage
+
+  override var item: Any {
+    get {
+      return self.image
+    }
+  }
+
+  override init(placeholderItem: Any) {
+    guard let image = placeholderItem as? UIImage else {
+      fatalError("DEBUG ::: Couldn't create image from provided item")
+    }
+
+    self.image = image
+    super.init(placeholderItem: placeholderItem)
+  }
+
+  @available(iOS 13.0, *)
+  override func activityViewControllerLinkMetadata(_ activityViewController: UIActivityViewController) -> LPLinkMetadata? {
+
+    let metadata = LPLinkMetadata()
+    metadata.title = "Result Image"
+
+    var thumbnail: NSSecureCoding = NSNull()
+    if let imageData = self.image.pngData() {
+      thumbnail = NSData(data: imageData)
+    }
+
+    metadata.imageProvider = NSItemProvider(item: thumbnail, typeIdentifier: "public.png")
+
+    return metadata
+  }
+
 }
