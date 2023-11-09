ad68adc066ba86655d4f9bea015148be6d6f570a
Hajime <impapa68@gmail.com>
2023-11-09 14:20:21 +0900

    refactor: swiftLint 준수(#15)

--- Projects/Common/Sources/Helper/ImageMetadataProvider.swift
+++ Projects/Common/Sources/Helper/ImageMetadataProvider.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  ImageMetadataProvider.swift
 //  Common
 //
 //  Created by Ha Jong Myeong on 11/9/23.
@@ -11,34 +11,35 @@ import UIKit
 import LinkPresentation
 
 public class ImageMetadataProvider: UIActivityItemProvider {
-  var image: UIImage
+    var image: UIImage
 
-  public override var item: Any {
-      return self.image
-  }
-
-  public override init(placeholderItem: Any) {
-    guard let image = placeholderItem as? UIImage else {
-      fatalError("DEBUG ::: Couldn't create image from provided item")
+    public override var item: Any {
+        return self.image
     }
 
-    self.image = image
-    super.init(placeholderItem: placeholderItem)
-  }
+    public override init(placeholderItem: Any) {
+        guard let image = placeholderItem as? UIImage else {
+            fatalError("DEBUG ::: Couldn't create image from provided item")
+        }
 
-  @available(iOS 13.0, *)
-  public override func activityViewControllerLinkMetadata(_ activityViewController: UIActivityViewController) -> LPLinkMetadata? {
-
-    let metadata = LPLinkMetadata()
-    metadata.title = "Result Image"
-
-    var thumbnail: NSSecureCoding = NSNull()
-    if let imageData = self.image.pngData() {
-      thumbnail = NSData(data: imageData)
+        self.image = image
+        super.init(placeholderItem: placeholderItem)
     }
 
-    metadata.imageProvider = NSItemProvider(item: thumbnail, typeIdentifier: "public.png")
+    @available(iOS 13.0, *)
+    public override func activityViewControllerLinkMetadata(
+        _ activityViewController: UIActivityViewController
+    ) -> LPLinkMetadata? {
+        let metadata = LPLinkMetadata()
+        metadata.title = "Result Image" // 팀 이름으로 변경예정
 
-    return metadata
-  }
+        var thumbnail: NSSecureCoding = NSNull()
+        if let imageData = self.image.pngData() {
+            thumbnail = NSData(data: imageData)
+        }
+
+        metadata.imageProvider = NSItemProvider(item: thumbnail, typeIdentifier: "public.png")
+
+        return metadata
+    }
 }
