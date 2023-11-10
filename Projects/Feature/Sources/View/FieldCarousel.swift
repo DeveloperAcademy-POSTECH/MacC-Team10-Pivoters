ef5424152fa38514de7b5f8131481d42be817db7
Hajime <impapa68@gmail.com>
2023-11-10 15:51:46 +0900

    feat: FieldCarousel(Snap) 구현 (#/19)

---
+++ Projects/Feature/Sources/View/FieldCarousel.swift
@@ -0,0 +1,70 @@
+//
+//  FIeldCarousel.swift
+//  Feature
+//
+//  Created by Ha Jong Myeong on 11/10/23.
+//  Copyright © 2023 com.pivoters. All rights reserved.
+//
+
+import SwiftUI
+
+public struct FieldCarousel<Content: View>: View {
+    public typealias PageIndex = Int
+
+    let pageCount: Int
+    let visibleEdgeSpace: CGFloat
+    let spacing: CGFloat
+    let content: (PageIndex) -> Content
+
+    @GestureState var dragOffset: CGFloat = 0
+    @State var currentIndex: Int = 0
+
+    public init(
+        pageCount: Int,
+        visibleEdgeSpace: CGFloat,
+        spacing: CGFloat,
+        @ViewBuilder content: @escaping (PageIndex) -> Content
+    ) {
+        self.pageCount = pageCount
+        self.visibleEdgeSpace = visibleEdgeSpace
+        self.spacing = spacing
+        self.content = content
+    }
+
+    public var body: some View {
+        GeometryReader { proxy in
+            let baseOffset: CGFloat = spacing + visibleEdgeSpace
+            let pageWidth: CGFloat = proxy.size.width - (visibleEdgeSpace + spacing) * 2
+            let offsetX: CGFloat = baseOffset
+                + CGFloat(currentIndex) * -pageWidth
+                + CGFloat(currentIndex) * -spacing
+                + dragOffset
+
+            HStack(spacing: spacing) {
+                ForEach(0..<pageCount, id: \.self) { pageIndex in
+                    self.content(pageIndex)
+                        .frame(
+                            width: pageWidth,
+                            height: proxy.size.height
+                        )
+                }
+                .contentShape(Rectangle())
+            }
+            .offset(x: offsetX)
+            .gesture(
+                DragGesture()
+                    .updating($dragOffset) { value, out, _ in
+                        out = value.translation.width
+                    }
+                    .onEnded { value in
+                        let offsetX = value.translation.width
+                        let progress = -offsetX / pageWidth
+                        let increment = Int(progress.rounded())
+
+                        currentIndex = max(min(currentIndex + increment, pageCount - 1), 0)
+                    }
+            )
+            .animation(.spring, value: offsetX)
+        }
+    }
+}
