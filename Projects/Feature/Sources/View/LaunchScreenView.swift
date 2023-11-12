a1714c8dbcd6f383c4a023c30663f163d4b19a68
Hajime <impapa68@gmail.com>
2023-11-13 04:56:00 +0900

    add: LaunchScreenVIew(#30)

---
+++ Projects/Feature/Sources/View/LaunchScreenView.swift
@@ -0,0 +1,19 @@
+//
+//  LaunchScreenView.swift
+//  Feature
+//
+//  Created by Ha Jong Myeong on 11/13/23.
+//  Copyright © 2023 com.pivoters. All rights reserved.
+//
+
+import SwiftUI
+
+struct LaunchScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchScreenView()
+}
