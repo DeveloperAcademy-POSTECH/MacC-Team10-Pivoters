b628fc4aafa2bf1c5b2cd3303e24bbd0e7d78803
Hajime <impapa68@gmail.com>
2023-11-13 05:16:59 +0900

    feat: LaunchScreen 구현 및 메인뷰 적용

--- Projects/Feature/Sources/View/LaunchScreenView.swift
+++ Projects/Feature/Sources/View/LaunchScreenView.swift
@@ -10,7 +10,26 @@ import SwiftUI
 
 struct LaunchScreenView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            LinearGradient(
+            stops: [
+            Gradient.Stop(color: Color(red: 0.74, green: 0.74, blue: 0.74), location: 0.00),
+            Gradient.Stop(color: Color(red: 0.74, green: 0.74, blue: 0.74).opacity(0), location: 1.00),
+            ],
+            startPoint: UnitPoint(x: 0.5, y: 0),
+            endPoint: UnitPoint(x: 0.5, y: 1))
+            .edgesIgnoringSafeArea(.all)
+
+            VStack {
+                Text("Linable")
+                    .font(.system(size: 20))
+                    .fontWeight(.bold)
+                Text("the most beautiful lineup maker")
+                    .font(Font.custom("SUIT", size: 14))
+                    .foregroundColor(Color(red: 0.3, green: 0.3, blue: 0.3))
+            }
+        }
+        .background(Color.white)
     }
 }
 
