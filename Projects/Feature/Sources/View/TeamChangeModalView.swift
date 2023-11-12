bd875101883469f8e8e7bde0a32aca427cde21db
Hajime <impapa68@gmail.com>
2023-11-12 17:15:19 +0900

    add: TeamChangeModalView(#27)

---
+++ Projects/Feature/Sources/View/TeamChangeModalView.swift
@@ -0,0 +1,17 @@
+//
+//  TeamChangeModalView.swift
+//  Feature
+//
+//  Created by Ha Jong Myeong on 11/12/23.
+//  Copyright © 2023 com.pivoters. All rights reserved.
+//
+
+import SwiftUI
+
+struct TeamChangeModalView: View {
+    var body: some View {
+        VStack {
+            Text("바텀 싵.")
+        }
+    }
+}
