1f34db5f53c4868d359ae4fd81f8f4c6bf2dfa15
Hajime <impapa68@gmail.com>
2023-11-16 05:03:28 +0900

    refactor: 변수 주석 추가(#47)

--- Projects/Feature/Sources/Observable/MainObservable.swift
+++ Projects/Feature/Sources/Observable/MainObservable.swift
@@ -10,13 +10,13 @@ import Foundation
 
 @Observable
 public final class MainObservable {
-    public var isSharing: Bool
-    public var isLoading: Bool
-    public var isShowTeamSheet: Bool
-    public var isShowEditSheet: Bool
-    public var currentIndex: Int
-    public var editSheetOffset: CGFloat
-    public var editSheetIndicatorOffset: CGFloat
+    public var isSharing: Bool // 공유 상태 여뷰
+    public var isLoading: Bool // 초기 로딩 상태 여뷰
+    public var isShowTeamSheet: Bool // 팀 교체 시트 표시 여뷰
+    public var isShowEditSheet: Bool // 편집 시트 표시 여부
+    public var currentIndex: Int // 현재 인덱스 값: carousel
+    public var editSheetOffset: CGFloat // 편집 시트 오프셋
+    public var editSheetIndicatorOffset: CGFloat // "밀어서 편집하기" 섹션 오프셋
 
     public init() {
         self.isSharing = false
