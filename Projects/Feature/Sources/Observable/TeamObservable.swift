cc7ff8e2095c18a88a7f1c235217b1892b0f206b
sozohoy <sozohoy@gmail.com>
2023-11-15 22:58:20 +0900

    feat: TeamObservable 구현(#51)

--- Projects/Feature/Sources/Observable/TeamObservable.swift
+++ Projects/Feature/Sources/Observable/TeamObservable.swift
@@ -6,31 +6,132 @@
 //  Copyright © 2023 com.pivoters. All rights reserved.
 //
 
-import Core
-import SwiftUI
+import Foundation
 
-class TeamObservable: ObservableObject {
-    @Published var currentTeam: Team
-    var mockTeams: [Team] = []
+import Core
+
+@Observable
+class TeamObservable {
+
+    var selectedTeam: String
+    var teams: [Team]
 
     init() {
-        self.currentTeam = Team(id: UUID(), teamName: "Default Team", subTitle: "Default Subtitle", lineup: [])
-        loadMockTeams()
-        if let firstMockTeam = mockTeams.first {
-            self.currentTeam = firstMockTeam
+        self.teams = MockData.teams
+        if let selectedTeamUUID = UserDefaults.standard.string(forKey: "selectedTeamUUID") {
+            self.selectedTeam = selectedTeamUUID
+        } else {
+            self.selectedTeam = MockData.initLineup[0].id.uuidString
         }
+        print(self.selectedTeam)
     }
 
-    func updateTeam(with team: Team) {
-        currentTeam = team
+    func addTeam() {
+
     }
 
-    func loadMockTeams() {
-        let team1 = Team(id: UUID(), teamName: "울산현대 FC", subTitle: "2023 아시안 챔피언스리그 결승전 선발 멤버", lineup: [])
-        let team2 = Team(id: UUID(), teamName: "첼시 FC", subTitle: "22/23 Season", lineup: [])
-        let team3 = Team(id: UUID(), teamName: "토트넘 핫스퍼 FC", subTitle: "22/24 Season", lineup: [])
-
-        mockTeams = [team1, team2, team3]
+    func selectTeam() {
     }
 
+}
+
+struct MockData {
+    static let teams: [Team] = [
+        Team(id: UUID(), teamName: "첼시 FC", subTitle: "2023 아시안 챔피언스 리그 결승전 선발 멤버", lineup: MockData.initLineup),
+        Team(id: UUID(), teamName: "토트넘 훗스퍼", subTitle: "2023 아시안 챔피언스 리그 결승전 선발 멤버", lineup: MockData.initLineup),
+        Team(id: UUID(), teamName: "갈라타사라이 현대 FC", subTitle: "2023 아시안 챔피언스 리그 결승전 선발 멤버", lineup: MockData.initLineup)
+    ]
+
+    static let initLineup: [Lineup] = [
+        Lineup(id: UUID(),
+               uniform: .plain,
+               formation: .eleven,
+               selectedTypeOfFormation: .football433,
+               players: MockData.player,
+               primaryColor: UniformColor(red: 0, green: 0, blue: 0),
+               secondaryColor: UniformColor(red: 255, green: 255, blue: 255)),
+        Lineup(id: UUID(),
+               uniform: .plain,
+               formation: .eleven,
+               selectedTypeOfFormation: .football433,
+               players: MockData.player,
+               primaryColor: UniformColor(red: 0, green: 0, blue: 0),
+               secondaryColor: UniformColor(red: 255, green: 255, blue: 255)),
+        Lineup(id: UUID(),
+               uniform: .plain,
+               formation: .eleven,
+               selectedTypeOfFormation: .football433,
+               players: MockData.player,
+               primaryColor: UniformColor(red: 0, green: 0, blue: 0),
+               secondaryColor: UniformColor(red: 255, green: 255, blue: 255))
+    ]
+
+    static let player: [Player] = [
+        Player(id: UUID(),
+               name: " ",
+               number: 1,
+               isGoalkeeper: true,
+               offset: OffsetValue(draggedOffset: CGSize(width: 0, height: 100),
+                                   accumulatedOffset: CGSize(width: 0, height: 100))),
+        Player(id: UUID(),
+               name: " ",
+               number: 2,
+               isGoalkeeper: false,
+               offset: OffsetValue(draggedOffset: CGSize(width: -75, height: 50),
+                                   accumulatedOffset: CGSize(width: -75, height: 50))),
+        Player(id: UUID(),
+               name: " ",
+               number: 3,
+               isGoalkeeper: false,
+               offset: OffsetValue(draggedOffset: CGSize(width: 75, height: 50),
+                                   accumulatedOffset: CGSize(width: 75, height: 50))),
+        Player(id: UUID(),
+               name: " ",
+               number: 4,
+               isGoalkeeper: false,
+               offset: OffsetValue(draggedOffset: CGSize(width: 140, height: 30),
+                                   accumulatedOffset: CGSize(width: 140, height: 30))),
+        Player(id: UUID(),
+               name: " ",
+               number: 5,
+               isGoalkeeper: false,
+               offset: OffsetValue(draggedOffset: CGSize(width: -140, height: 30),
+                                   accumulatedOffset: CGSize(width: -140, height: 30))),
+        Player(id: UUID(),
+               name: " ",
+               number: 6,
+               isGoalkeeper: false,
+               offset: OffsetValue(draggedOffset: CGSize(width: 0, height: 20),
+                                   accumulatedOffset: CGSize(width: 0, height: 20))),
+        Player(id: UUID(),
+               name: " ",
+               number: 7,
+               isGoalkeeper: false,
+               offset: OffsetValue(draggedOffset: CGSize(width: 70, height: -20),
+                                   accumulatedOffset: CGSize(width: 70, height: -20))),
+        Player(id: UUID(),
+               name: " ",
+               number: 8,
+               isGoalkeeper: false,
+               offset: OffsetValue(draggedOffset: CGSize(width: -70, height: -20),
+                                   accumulatedOffset: CGSize(width: -70, height: -20))),
+        Player(id: UUID(),
+               name: " ",
+               number: 9,
+               isGoalkeeper: false,
+               offset: OffsetValue(draggedOffset: CGSize(width: 0, height: -80),
+                                   accumulatedOffset: CGSize(width: 0, height: -80))),
+        Player(id: UUID(),
+               name: " ",
+               number: 10,
+               isGoalkeeper: false,
+               offset: OffsetValue(draggedOffset: CGSize(width: 130, height: -80),
+                                   accumulatedOffset: CGSize(width: 130, height: -80))),
+        Player(id: UUID(),
+               name: " ",
+               number: 11,
+               isGoalkeeper: false,
+               offset: OffsetValue(draggedOffset: CGSize(width: -130, height: -80),
+                                   accumulatedOffset: CGSize(width: -130, height: -80)))
+    ]
 }
