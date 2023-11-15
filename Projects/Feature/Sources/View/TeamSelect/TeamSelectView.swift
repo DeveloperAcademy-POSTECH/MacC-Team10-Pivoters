b17ed812ea9e3dbbeb193eb2e4281dcf8e8d9790
sozohoy <sozohoy@gmail.com>
2023-11-15 23:52:54 +0900

    feat: TeamSelectView로 이름 변경, UI 구현(#51)

--- Projects/Feature/Sources/View/TeamSelect/TeamSelectView.swift
+++ Projects/Feature/Sources/View/TeamSelect/TeamSelectView.swift
@@ -12,28 +12,57 @@ import Core
 import Common
 
 struct TeamSelectView: View {
+
+    @Environment(\.dismiss) var dismiss
     @State var observable: TeamObservable
 
     var body: some View {
-        VStack {
+        VStack(spacing: 0) {
+            // MARK: 상단 Dismiss
+            HStack {
+                Spacer()
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "x.circle.fill")
+                        .renderingMode(.template)
+                        .foregroundStyle(Color.colorLightGray)
+                        .frame(width: 30, height: 30)
+                }
+            }
+            .padding(.top, 16)
+            .padding(.trailing, 16)
+            // MARK: 하단 List
             teamList
         }
+        .background(.regularMaterial)
         .presentationDetents([.fraction(0.5)])
-        .presentationBackground(.regularMaterial)
+        .presentationDragIndicator(.visible)
+        .sheet(isPresented: $observable.isPresented) {
+            TeamCreateView()
+                .presentationDetents([.fraction(0.5)])
+                .presentationBackground(.regularMaterial)
+        }
     }
 
     var teamList: some View {
         List {
             ForEach(observable.teams, id: \.id) { team in
-                TeamCell(team: team, cellType: .select)
-                    .teamCellViewModifier()
-                    .onTapGesture {
-                    }
-            }
-            TeamCell(team: nil, cellType: .add)
+                TeamCell(team: team,
+                         cellType: .select,
+                         isSelected: team.id.uuidString == observable.selectedTeam)
                 .teamCellViewModifier()
                 .onTapGesture {
+                    print(team.id)
                 }
+            }
+            TeamCell(team: nil,
+                     cellType: .add,
+                     isSelected: false)
+            .teamCellViewModifier()
+            .onTapGesture {
+                observable.isPresented.toggle()
+            }
         }
         .padding(.top, 24)
         .padding(.horizontal, 60)
@@ -43,32 +72,4 @@ struct TeamSelectView: View {
         .scrollContentBackground(.hidden)
     }
 
-}
-
-enum TeamCellType {
-    case add
-    case select
-}
-
-struct TeamCell: View {
-
-    let team: Team?
-    let cellType: TeamCellType
-
-    var body: some View {
-        HStack(alignment: .center) {
-            Image(asset: CommonAsset.uniform)
-                .resizable()
-                .frame(width: 36, height: 36)
-                .padding(.leading, 34)
-            Spacer()
-            Text(cellType == .select ? team!.teamName : "+ 새로운 팀 추가")
-                .foregroundStyle(cellType == .select ? Color.colorBlack : Color.colorLightGray)
-                .font(.Pretendard.black14.font)
-                .fontWeight(.bold)
-            Spacer()
-        }
-        .frame(height: 60)
-        .background(.white)
-    }
 }
