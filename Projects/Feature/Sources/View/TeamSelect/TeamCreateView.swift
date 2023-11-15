f37dc839166565a9e3ff7cde9421909b9f37b753
sozohoy <sozohoy@gmail.com>
2023-11-16 01:29:48 +0900

    feat: 팀생성 뷰 구현(#51)

--- Projects/Feature/Sources/View/TeamSelect/TeamCreateView.swift
+++ Projects/Feature/Sources/View/TeamSelect/TeamCreateView.swift
@@ -9,8 +9,50 @@
 import SwiftUI
 
 struct TeamCreateView: View {
+
+    @Environment(\.dismiss) var dismiss
+    @State var teamName: String = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            HStack {
+                Spacer()
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "x.circle.fill")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundStyle(Color.colorLightGray)
+                        .frame(width: 25, height: 25)
+                }
+            }
+            .padding(.top, 16)
+            .padding(.trailing, 16)
+
+            TextField("팀명", text: $teamName)
+                .frame(height: 55)
+                .foregroundStyle(Color.colorBlack)
+                .font(.Pretendard.headerNormal.font)
+                .textFieldStyle(PlainTextFieldStyle())
+                .padding(.horizontal, 24)
+            Divider()
+                .padding(.horizontal, 24)
+            Spacer()
+            Button {
+                // MARK: addTeam
+            } label: {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundStyle(Color.indigo)
+                    .frame(height: 60)
+                    .padding(.horizontal, 20)
+                    .overlay {
+                        Text("추가")
+                    }
+            }
+            .padding(.bottom, 20)
+        }
+
     }
 }
 
