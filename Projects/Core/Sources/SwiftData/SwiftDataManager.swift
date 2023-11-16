bf7d97f2545b8a18da909e0bad60374274126738
sozohoy <sozohoy@gmail.com>
2023-11-17 03:22:09 +0900

    feat: teamContainer 수정(#55)

--- Projects/Core/Sources/SwiftData/SwiftDataManager.swift
+++ Projects/Core/Sources/SwiftData/SwiftDataManager.swift
@@ -9,14 +9,19 @@
 import Foundation
 import SwiftData
 
-///TODO: 이후에 TEAM으로 수정해야 함.
+/// TODO: 이후에 TEAM으로 수정해야 함.
 @MainActor
 public let teamContainer: ModelContainer = {
     do {
         let container = try ModelContainer(for: RefactoredTeam.self)
         let context = container.mainContext
         if try context.fetch(FetchDescriptor<RefactoredTeam>()).isEmpty {
-            container.mainContext.insert(RefactoredTeam(id: UUID(), teamName: "새 팀 1", subTitle: "새 서브 타이틀"))
+            container.mainContext.insert(RefactoredTeam(id: UUID(),
+                                                        teamName: "새 팀 1",
+                                                        subTitle: "새 서브 타이틀",
+                                                        isSelected: true,
+                                                        createdAt: Date(),
+                                                        updatedAt: Date()))
         }
         return container
     } catch {
@@ -45,7 +50,7 @@ public final class SwiftDataManager<T: PersistentModel> {
             fatalError(error.localizedDescription)
         }
     }
-    
+
     public func saveItems() {
         do {
             try modelContext.save()
