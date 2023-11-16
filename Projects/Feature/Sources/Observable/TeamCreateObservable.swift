684ec4f9c6214ff408ef20c17f5eef3436d4a529
sozohoy <sozohoy@gmail.com>
2023-11-17 03:23:36 +0900

    feat: 팀 생성 기능 구현(#55)

--- Projects/Feature/Sources/Observable/TeamCreateObservable.swift
+++ Projects/Feature/Sources/Observable/TeamCreateObservable.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+import SwiftData
+
+import Core
+
+class TeamCreateObservable {
+    private let modelContext: ModelContext
+    var teamName: String = ""
+
+    @MainActor
+    init(modelContext: ModelContext) {
+        self.modelContext = modelContext
+    }
+
+    func createTeam() {
+        guard self.teamName != "" else { return }
+        let team = RefactoredTeam(id: UUID(),
+                                  teamName: teamName,
+                                  subTitle: "temp",
+                                  isSelected: false,
+                                  createdAt: Date(),
+                                  updatedAt: Date())
+        modelContext.insert(team)
+    }
+}
