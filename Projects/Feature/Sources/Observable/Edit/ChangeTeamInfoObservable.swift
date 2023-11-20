cd36d57668a62c367664a84351a382a2091e0840
sozohoy <sozohoy@gmail.com>
2023-11-21 03:03:32 +0900

    chore: 폴더 이동(#71)

---
+++ Projects/Feature/Sources/Observable/Edit/ChangeTeamInfoObservable.swift
@@ -0,0 +1,9 @@
+//
+//  ChangeTeamInfoObservable.swift
+//  Feature
+//
+//  Created by 한지석 on 11/21/23.
+//  Copyright © 2023 com.pivoters. All rights reserved.
+//
+
+import Foundation
---
+++ Projects/Feature/Sources/View/Edit/ChangeTeamInfoView.swift
@@ -0,0 +1,19 @@
+//
+//  ChangeTeamInfoView.swift
+//  Feature
+//
+//  Created by 한지석 on 11/21/23.
+//  Copyright © 2023 com.pivoters. All rights reserved.
+//
+
+import SwiftUI
+
+struct ChangeTeamInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChangeTeamInfoView()
+}
