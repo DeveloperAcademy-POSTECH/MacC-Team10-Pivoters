93c3f244d97d2ffa80061e19dad407e7c3be2c1d
sozohoy <sozohoy@gmail.com>
2023-11-21 03:09:39 +0900

    feat: ChangeTeamInfoObservable 구현(#71)

--- Projects/Feature/Sources/Observable/Edit/ChangeTeamInfoObservable.swift
+++ Projects/Feature/Sources/Observable/Edit/ChangeTeamInfoObservable.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+import Core
+
+@Observable
+final class ChangeTeamInfoObservable {
+    let changeTeamInfo: ChangeTeamInfo
+    var name: String
+    var team: Team?
+    var lineup: Lineup?
+
+    init(changeTeamInfo: ChangeTeamInfo,
+         name: String,
+         team: Team? = nil,
+         lineup: Lineup? = nil) {
+        self.changeTeamInfo = changeTeamInfo
+        self.name = name
+        self.team = team
+        self.lineup = lineup
+    }
+
+    func changeName() {
+        switch changeTeamInfo {
+        case .team:
+            self.team?.teamName = self.name
+        case .squad:
+            self.lineup?.lineupName = self.name
+        }
+    }
+
+}
