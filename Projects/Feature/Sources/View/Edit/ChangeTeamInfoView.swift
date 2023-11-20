7dfec257f0e4ba8f910ac5a1ee7a96ed1ce35149
sozohoy <sozohoy@gmail.com>
2023-11-21 03:07:48 +0900

    feat: ChangeTeamInfoView 구현(#71)

--- Projects/Feature/Sources/View/Edit/ChangeTeamInfoView.swift
+++ Projects/Feature/Sources/View/Edit/ChangeTeamInfoView.swift
@@ -8,12 +8,66 @@
 
 import SwiftUI
 
-struct ChangeTeamInfoView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+enum ChangeTeamInfo {
+    case team
+    case squad
 }
 
-#Preview {
-    ChangeTeamInfoView()
+struct ChangeTeamInfoView: View {
+
+    @Environment(\.dismiss) var dismiss
+    @State var observable: ChangeTeamInfoObservable
+    let changeTeamInfo: ChangeTeamInfo
+
+    init(observable: ChangeTeamInfoObservable,
+         changeTeamInfo: ChangeTeamInfo) {
+        self.changeTeamInfo = changeTeamInfo
+        let observable = observable
+        _observable = State(initialValue: observable)
+    }
+
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack {
+                Spacer()
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "x.circle.fill")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundStyle(Color.colorLightGray)
+                        .frame(width: 25, height: 25)
+                }
+            }
+            .padding(.top, 16)
+            .padding(.trailing, 16)
+
+            TextField(changeTeamInfo == .team ? "팀 이름" : "스쿼드 이름", text: $observable.name)
+                .frame(height: 55)
+                .foregroundStyle(Color.colorBlack)
+                .font(.Pretendard.headerNormal.font)
+                .textFieldStyle(PlainTextFieldStyle())
+                .padding(.horizontal, 24)
+            Divider()
+                .padding(.horizontal, 24)
+            Spacer()
+            Button {
+                // MARK: createTeam
+                observable.changeName()
+                dismiss()
+            } label: {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundStyle(Color(red: 0 / 255, green: 122 / 255, blue: 255 / 255))
+                    .frame(height: 60)
+                    .padding(.horizontal, 20)
+                    .overlay {
+                        Text("변경")
+                            .font(.Pretendard.headerNormal.font)
+                            .foregroundColor(.colorWhite)
+                    }
+            }
+            .padding(.bottom, 20)
+        }
+    }
 }
