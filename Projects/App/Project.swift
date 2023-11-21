872259571d540fb05d3ed2ef70ceb29361d5725d
sozohoy <sozohoy@gmail.com>
2023-11-21 23:28:44 +0900

    chore: InfoPlist 수정, bundle id 수정(#75)

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -13,17 +13,19 @@ private let moduleName = "App"
 let infoPlist: [String: Plist.Value] = [
     "CFBundleShortVersionString": "1.0.0",
     "CFBundleVersion": "1",
-    "CFBundleDisplayName" : "Linable",
+    "CFBundleDisplayName": "Linable",
     "UIMainStoryboardFile": "",
     "UILaunchStoryboardName": "LaunchScreen",
     "NSPhotoLibraryAddUsageDescription" : "to let you export images to photo library",
     "CFBundleAllowMixedLocalizations" : "true"
+    "UISupportedInterfaceOrientations": ["UIInterfaceOrientationPortrait"],
+    "LSApplicationCategoryType": "public.app-category.sports"
 ]
 
 let project = Project.makeModule(name: moduleName,
                                  platform: .iOS,
                                  product: .app,
-                                 bundleId: "app",
+                                 bundleId: "app.linable",
                                  infoPlist: .extendingDefault(with: infoPlist),
                                  resources: ["Resources/**"],
                                  dependencies: [
