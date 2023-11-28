c3676e3fea081892497674aa9921cb8757cb5cd8
eemdeeks <prince9088@gmail.com>
2023-11-28 18:21:45 +0900

    feat: 뷰 기본 틀 수정(#102)

--- Projects/Feature/Sources/View/Edit/PlayerSelectionView.swift
+++ Projects/Feature/Sources/View/Edit/PlayerSelectionView.swift
@@ -13,34 +13,36 @@ import Core
 
 struct PlayerSelectionView: View {
     @State var observable: PlayerSelectionObservable
+    let columns = [GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible()), GridItem(.flexible())]
 
     var body: some View {
-        List {
-            addPlayerCell()
-                .listRowSeparator(.hidden)
-            ForEach(observable.humans.indices, id: \.hashValue) { index in
-                PlayerCell(human: $observable.humans[index],
-                           observable: observable)
-                    .listRowSeparator(.hidden)
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                addPlayerCell()
+                ForEach(observable.humans.indices, id: \.hashValue) { index in
+                    PlayerCell(observable: observable, human: observable.humans[index])
+                }
             }
+            .task(id: observable.lineup.selectionPlayerIndex, {
+                observable.isEditedHuman = nil
+            })
         }
-        .task(id: observable.lineup.selectionPlayerIndex, {
-            observable.isEditedHuman = nil
-        })
-        .listStyle(.plain)
-        .listRowSpacing(8)
     }
 
     func addPlayerCell() -> some View {
-        HStack {
-            Image(systemName: "plus.circle.fill")
-            Text(String(localized: "Add Player"))
-                .font(.Pretendard.regular12.font)
-                .padding(.horizontal, 10)
-            Spacer()
+        VStack {
+            ZStack {
+                OverlapUniform(uniform: observable.lineup.uniform,
+                               uniformSize: 50,
+                               primaryColor: observable.lineup.primaryColor,
+                               secondaryColor: observable.lineup.secondaryColor,
+                               isSelected: false,
+                               isGoalkeeper: false)
+                .shadow(color: .black.opacity(0.1), radius: 2, x: 0, y: 4)
+                Image(systemName: "plus")
+            }
+            Text("추가")
         }
-        .background(Color.white.opacity(0.1))
-        .padding(.horizontal, 10)
         .onTapGesture {
             observable.addPlayer()
         }
@@ -48,72 +50,93 @@ struct PlayerSelectionView: View {
 }
 
 struct PlayerCell: View {
-    @Binding var human: Human
     var observable: PlayerSelectionObservable
-    let limitLength: Int = 15
+    var human: Human
 
     var body: some View {
-        HStack {
-            VStack {
-                if observable.lineup.players.firstIndex(where: { $0.human?.id == human.id}) != nil {
-                    Image(systemName: "checkmark.circle")
-                        .foregroundStyle(Color(asset: CommonAsset.tintColor))
-                } else {
-                    Image(systemName: "checkmark.circle")
-                        .foregroundStyle(Color.gray)
-                }
-            }
-            .onTapGesture {
-                if observable.isEditedHuman != human.id {
-                    observable.selectPlayer(human)
-                    observable.isEditedHuman = nil
-                }
-            }
-            VStack {
-                TextField(String(localized: "Player name"), text: $human.name, onCommit: {
-                    observable.isEditedHuman = nil
-                })
-                .submitLabel(.done)
-                .font(.Pretendard.regular12.font)
-                .textFieldStyle(.automatic)
-                .onReceive(human.name.publisher.collect()) { newText in
-                    if newText.count > limitLength {
-                        human.name = String(newText.prefix(limitLength))
-                    }
-                }
-                .disabled(observable.isEditedHuman != human.id)
-                Rectangle()
-                    .frame(height: 1)
-                    .foregroundColor(observable.isEditedHuman == human.id ? .gray: .white)
-            }
-            .offset(y: 3)
-            .background(Color.white.opacity(0.1))
-            .padding(.horizontal, 10)
-            .onTapGesture {
-                if observable.isEditedHuman != human.id {
-                    observable.selectPlayer(human)
-                    observable.isEditedHuman = nil
-                }
-            }
-            .task(id: human.name) {
-                observable.lineup.trigger = Int.random(in: 0...10)
-            }
-            Image(systemName: "square.and.pencil")
-                .foregroundStyle(Color.gray)
-                .onTapGesture {
-                    if observable.isEditedHuman == human.id {
-                        observable.isEditedHuman = nil
-                        if human.name.isEmpty {
-                            human.name = String(localized: "Player")
-                        }
-                    } else {
-                        observable.isEditedHuman = human.id
-                    }
-                }
+        VStack {
+            OverlapUniform(uniform: observable.lineup.uniform,
+                           uniformSize: 50,
+                           primaryColor: observable.lineup.primaryColor,
+                           secondaryColor: observable.lineup.secondaryColor,
+                           isSelected: false,
+                           isGoalkeeper: false)
+            .shadow(color: .black.opacity(0.1), radius: 2, x: 0, y: 4)
+            Text("\(human.name)")
         }
-        .padding(.horizontal, 10)
+        .onTapGesture {
+            observable.selectPlayer(human)
+        }
     }
 }
+
+//struct PlayerCell: View {
+//    @Binding var human: Human
+//    var observable: PlayerSelectionObservable
+//    let limitLength: Int = 15
+//
+//    var body: some View {
+//        HStack {
+//            VStack {
+//                if observable.lineup.players.firstIndex(where: { $0.human?.id == human.id}) != nil {
+//                    Image(systemName: "checkmark.circle")
+//                        .foregroundStyle(Color(asset: CommonAsset.tintColor))
+//                } else {
+//                    Image(systemName: "checkmark.circle")
+//                        .foregroundStyle(Color.gray)
+//                }
+//            }
+//            .onTapGesture {
+//                if observable.isEditedHuman != human.id {
+//                    observable.selectPlayer(human)
+//                    observable.isEditedHuman = nil
+//                }
+//            }
+//            VStack {
+//                TextField(String(localized: "Player name"), text: $human.name, onCommit: {
+//                    observable.isEditedHuman = nil
+//                })
+//                .submitLabel(.done)
+//                .font(.Pretendard.regular12.font)
+//                .textFieldStyle(.automatic)
+//                .onReceive(human.name.publisher.collect()) { newText in
+//                    if newText.count > limitLength {
+//                        human.name = String(newText.prefix(limitLength))
+//                    }
+//                }
+//                .disabled(observable.isEditedHuman != human.id)
+//                Rectangle()
+//                    .frame(height: 1)
+//                    .foregroundColor(observable.isEditedHuman == human.id ? .gray: .white)
+//            }
+//            .offset(y: 3)
+//            .background(Color.white.opacity(0.1))
+//            .padding(.horizontal, 10)
+//            .onTapGesture {
+//                if observable.isEditedHuman != human.id {
+//                    observable.selectPlayer(human)
+//                    observable.isEditedHuman = nil
+//                }
+//            }
+//            .task(id: human.name) {
+//                observable.lineup.trigger = Int.random(in: 0...10)
+//            }
+//            Image(systemName: "square.and.pencil")
+//                .foregroundStyle(Color.gray)
+//                .onTapGesture {
+//                    if observable.isEditedHuman == human.id {
+//                        observable.isEditedHuman = nil
+//                        if human.name.isEmpty {
+//                            human.name = String(localized: "Player")
+//                        }
+//                    } else {
+//                        observable.isEditedHuman = human.id
+//                    }
+//                }
+//        }
+//        .padding(.horizontal, 10)
+//    }
+//}
 
 extension View {
     func endTextEditing() {
