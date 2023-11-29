9d30d24f52a29d7da3ae80a21d60486b83522183
eemdeeks <prince9088@gmail.com>
2023-11-29 21:47:43 +0900

    feat: 선수 추가 기능 구현(#102)

--- Projects/Feature/Sources/Observable/Edit/AddPlayerObservable.swift
+++ Projects/Feature/Sources/Observable/Edit/AddPlayerObservable.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+import Combine
+
+import Core
+
+class AddPlayerObservable: ObservableObject {
+    @Published var playerName: String
+    @Published var isButtonEnabled: Bool
+    var team: Team
+
+    var cancellables = Set<AnyCancellable>()
+
+    private var validTextfieldPublisher: AnyPublisher<Bool, Never> {
+        $playerName
+            .map {
+                $0.count > 0
+            }
+            .eraseToAnyPublisher()
+    }
+
+    init(playerName: String = "", isButtonEnabled: Bool = false, team: Team) {
+        self.playerName = playerName
+        self.isButtonEnabled = isButtonEnabled
+        self.team = team
+
+        validTextfieldPublisher
+            .receive(on: RunLoop.main)
+            .map { $0 ? true : false }
+            .sink { self.isButtonEnabled = $0 }
+            .store(in: &cancellables)
+    }
+
+    func addPlayer() {
+        team.teamMembers.insert(InitTeamContainer.makeHuman(name: playerName,
+                                                            backNumber: 1),
+                                at: 0)
+    }
+}
