4e1f218953ed14bd67275f34f680aca0ee2e7b8e
sozohoy <sozohoy@gmail.com>
2023-11-29 17:52:49 +0900

    add: SheetModifier(#104)

---
+++ Projects/Common/Sources/DesignSystem/ViewModifier/SheetModifier.swift
@@ -0,0 +1,19 @@
+//
+//  SheetModifier.swift
+//  Common
+//
+//  Created by 한지석 on 11/29/23.
+//  Copyright © 2023 com.pivoters. All rights reserved.
+//
+
+import SwiftUI
+
+struct SheetModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SheetModifier()
+}
