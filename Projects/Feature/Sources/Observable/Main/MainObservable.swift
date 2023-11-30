d28550a8faa292f4f68e2785508864510227488c
sozohoy <sozohoy@gmail.com>
2023-11-30 17:24:27 +0900

    feat: 메인뷰 프로퍼티 추가(#104)

--- Projects/Feature/Sources/Observable/Main/MainObservable.swift
+++ Projects/Feature/Sources/Observable/Main/MainObservable.swift
@@ -7,10 +7,11 @@
 //
 
 import Foundation
-import UIKit
+import SwiftUI
 
 @Observable
 final class MainObservable {
+
     var isSharing: Bool // 공유 상태 여뷰
     var isLoading: Bool // 초기 로딩 상태 여뷰
     var isShowTeamSheet: Bool // 팀 교체 시트 표시 여뷰
@@ -19,6 +20,12 @@ final class MainObservable {
     var editSheetOffset: CGFloat // 편집 시트 오프셋
     var editSheetIndicatorOffset: CGFloat // "밀어서 편집하기" 섹션 오프셋
     var editType: EditType
+    var currentPresentationDetent: PresentationDetent = .fraction(0.16)
+    let presentationDetents: Set<PresentationDetent> = [.fraction(0.16), .fraction(0.48)]
+    let editHeight = UIScreen.main.bounds.size.height * 0.43
+    let defaultHeight = UIScreen.main.bounds.size.height * 0.11
+    let maxDragHeight = UIScreen.main.bounds.height / 3
+
 
     init() {
         self.isSharing = false
