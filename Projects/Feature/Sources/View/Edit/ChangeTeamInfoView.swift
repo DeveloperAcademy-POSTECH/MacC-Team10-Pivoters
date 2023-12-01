91fa1251a4625f765a25e51695ed3fa7fda96669
sozohoy <sozohoy@gmail.com>
2023-12-01 09:54:39 +0900

    feat: 팀 변경 디자인 수정(#104)

--- Projects/Feature/Sources/View/Edit/ChangeTeamInfoView.swift
+++ Projects/Feature/Sources/View/Edit/ChangeTeamInfoView.swift
@@ -28,61 +28,57 @@ struct ChangeTeamInfoView: View {
     }
 
     var body: some View {
-        VStack(spacing: 0) {
-            HStack {
-                Text(changeTeamInfo == .team ? String(localized: "Change Team Name") : String(localized: "Change Lineup Name"))
+        ZStack {
+            Color(uiColor: .systemGray5)
+                .ignoresSafeArea()
+            VStack(spacing: 0) {
+                HStack {
+                    Text(changeTeamInfo == .team ?
+                         String(localized: "Change Team Name")
+                         : String(localized: "Change Lineup Name"))
                     .font(.Pretendard.title.font)
                     .foregroundColor(.colorBlack)
-                    .padding(.top, 8)
+                    Spacer()
+                }
+                .padding(.top, 0)
+                .padding(.horizontal, 16)
+
+                TextField(changeTeamInfo == .team ? String(localized: "Team Name") : String(localized: "Lineup Name"), text: $observable.name)
+                    .frame(height: 55)
+                    .foregroundStyle(Color.colorBlack)
+                    .font(.Pretendard.headerNormal.font)
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .padding(.horizontal, 16)
+                    .padding(.top, 16)
+                    .onReceive(observable.name.publisher.collect()) {
+                        if $0.count > 20 {
+                            observable.name = String($0.prefix(20))
+                        }
+                    }
+                    .onAppear {
+                        UITextField.appearance().clearButtonMode = .whileEditing
+                    }
+                Divider()
+                    .padding(.horizontal, 16)
                 Spacer()
                 Button {
+                    // MARK: createTeam
+                    observable.changeName()
                     dismiss()
                 } label: {
-                    Image(systemName: "x.circle.fill")
-                        .resizable()
-                        .renderingMode(.template)
-                        .foregroundStyle(Color.colorLightGray)
-                        .frame(width: 25, height: 25)
+                    RoundedRectangle(cornerRadius: 12)
+                        .foregroundStyle(observable.isButtonEnabled ? Color.colorBlue : Color.colorLightGray)
+                        .frame(height: 60)
+                        .padding(.horizontal, 20)
+                        .overlay {
+                            Text(String(localized: "Change"))
+                                .font(.Pretendard.headerNormal.font)
+                                .foregroundColor(.colorWhite)
+                        }
                 }
+                .padding(.bottom, 20)
+                .disabled(!observable.isButtonEnabled)
             }
-            .padding(.top, 16)
-            .padding(.horizontal, 16)
-
-            TextField(changeTeamInfo == .team ? String(localized: "Team Name") : String(localized: "Lineup Name"), text: $observable.name)
-                .frame(height: 55)
-                .foregroundStyle(Color.colorBlack)
-                .font(.Pretendard.headerNormal.font)
-                .textFieldStyle(PlainTextFieldStyle())
-                .padding(.horizontal, 16)
-                .padding(.top, 16)
-                .onReceive(observable.name.publisher.collect()) {
-                    if $0.count > 20 {
-                        observable.name = String($0.prefix(20))
-                    }
-                }
-                .onAppear {
-                    UITextField.appearance().clearButtonMode = .whileEditing
-                }
-            Divider()
-                .padding(.horizontal, 16)
-            Spacer()
-            Button {
-                // MARK: createTeam
-                observable.changeName()
-                dismiss()
-            } label: {
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundStyle(observable.isButtonEnabled ? Color.colorBlue : Color.colorLightGray)
-                    .frame(height: 60)
-                    .padding(.horizontal, 20)
-                    .overlay {
-                        Text(String(localized: "Change"))
-                            .font(.Pretendard.headerNormal.font)
-                            .foregroundColor(.colorWhite)
-                    }
-            }
-            .padding(.bottom, 20)
-            .disabled(!observable.isButtonEnabled)
         }
         .submitLabel(.done)
         .onSubmit {
