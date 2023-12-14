7dd8750e42d351f82aee102381237ef3bea697fe
sozohoy <sozohoy@gmail.com>
2023-12-14 18:24:09 +0900

    feat: Team 모델 수정(#116)
    - VersionedSchema 적용, Relationship 적용, 프로퍼티 네이밍 수정

--- Projects/Core/Sources/Models/Team.swift
+++ Projects/Core/Sources/Models/Team.swift
@@ -10,29 +10,33 @@ import Foundation
 import SwiftUI
 import SwiftData
 
-//@Model
-//public final class Team {
-//    public let id: UUID
-//    public var teamName: String
-//    public var isSelected: Bool
-//    public let createdAt: Date
-//    public var updatedAt: Date
-//    public var lineup: [Lineup]
-//    public var teamMembers: [Human]
-//
-//    public init(id: UUID,
-//                teamName: String,
-//                isSelected: Bool,
-//                createdAt: Date,
-//                updatedAt: Date,
-//                lineup: [Lineup],
-//                addedPlayers: [Human]) {
-//        self.id = id
-//        self.teamName = teamName
-//        self.isSelected = isSelected
-//        self.createdAt = createdAt
-//        self.updatedAt = updatedAt
-//        self.lineup = lineup
-//        self.teamMembers = addedPlayers
-//    }
-//}
+extension LinableSchemaV1 {
+
+    @Model
+    public final class Team {
+        public let id: UUID
+        public var teamName: String
+        public var isSelected: Bool
+        public let createdAt: Date
+        public var updatedAt: Date
+        @Relationship(deleteRule: .cascade) public var lineup: [Lineup]
+        @Relationship(deleteRule: .cascade) public var teamPlayers: [TeamPlayer]
+
+        public init(id: UUID,
+                    teamName: String,
+                    isSelected: Bool,
+                    createdAt: Date,
+                    updatedAt: Date,
+                    lineup: [Lineup],
+                    teamPlayers: [TeamPlayer]) {
+            self.id = id
+            self.teamName = teamName
+            self.isSelected = isSelected
+            self.createdAt = createdAt
+            self.updatedAt = updatedAt
+            self.lineup = lineup
+            self.teamPlayers = teamPlayers
+        }
+    }
+
+}
