81f80303938f71d0cbb1b9a149348b69d2f426d8
eemdeeks <prince9088@gmail.com>
2024-01-20 18:44:40 +0900

    add: Player 엔티티 구현(#128)

---
+++ Projects/Domain/Sources/Entity/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  Domain
+//
+//  Created by 박승찬 on 1/20/24.
+//  Copyright © 2024 com.pivoters. All rights reserved.
+//
+
+import Foundation
+
+public struct Player {
+    public let id: UUID
+    public var name: String
+    public var backNumber: Int
+}
