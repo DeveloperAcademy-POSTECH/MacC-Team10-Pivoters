d19f23787294cf32fc3a10ad3374a44b15835577
sozohoy <sozohoy@gmail.com>
2024-01-21 18:33:43 +0900

    add: LineupSchemaRepository, UseCase(#129)

---
+++ Projects/Domain/Sources/Repositories/LineupSchemaRepositoryInterface.swift
@@ -0,0 +1,9 @@
+//
+//  LineupSchemaRepositoryInterface.swift
+//  Domain
+//
+//  Created by 한지석 on 1/21/24.
+//  Copyright © 2024 com.pivoters. All rights reserved.
+//
+
+import Foundation
---
+++ Projects/Domain/Sources/UseCase/Lineup/FetchLineupUseCase.swift
@@ -0,0 +1,9 @@
+//
+//  FetchLineupUseCase.swift
+//  Domain
+//
+//  Created by 한지석 on 1/21/24.
+//  Copyright © 2024 com.pivoters. All rights reserved.
+//
+
+import Foundation
