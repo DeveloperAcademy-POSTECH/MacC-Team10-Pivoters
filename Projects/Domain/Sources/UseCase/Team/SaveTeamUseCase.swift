df250f47813879beb236ee4d3829284aa33db2ae
sozohoy <sozohoy@gmail.com>
2024-01-21 17:53:04 +0900

    feat: SaveUseCase 임시 구현(#129)

--- Projects/Domain/Sources/UseCase/Team/SaveTeamUseCase.swift
+++ Projects/Domain/Sources/UseCase/Team/SaveTeamUseCase.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+public protocol SaveTeamUseCase {
+    func excute(
+        name: String,
+        id: UUID?
+    )
+}
+
+public final class DefaultSaveTeamUseCase: SaveTeamUseCase {
+
+    private let teamSchemaRepository: TeamSchemaRepositoryInterface
+
+    public init(teamSchemaRepository: TeamSchemaRepositoryInterface) {
+        self.teamSchemaRepository = teamSchemaRepository
+    }
+
+    public func excute(name: String, id: UUID?) {
+        teamSchemaRepository.saveName(
+            name: name,
+            id: id
+        )
+    }
+
+}
