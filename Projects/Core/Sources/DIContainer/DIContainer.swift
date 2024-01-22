7df85f0cd4534b519147f5b48346b3db8b96f67b
sozohoy <sozohoy@gmail.com>
2024-01-22 19:38:38 +0900

    feat: DIContainer 구현(#129)

--- Projects/Core/Sources/DIContainer/DIContainer.swift
+++ Projects/Core/Sources/DIContainer/DIContainer.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+
+public final class DIContainer {
+
+    // MARK: - 싱글톤
+
+    public static let shared = DIContainer()
+
+    private init() { }
+
+    // MARK: - 서비스, 객체의 인스턴스들을 관리하는 공간
+
+    public var services: [String: Any] = [:]
+
+    // MARK: - 객체를 services에 등록하는 메소드
+
+    public func register<Service>(type: Service.Type, _ object: Service) {
+        services["\(type)"] = object
+    }
+
+    public func resolve<Service>(type: Service.Type) -> Service {
+        guard let object = services["\(type)"] as? Service else {
+            fatalError("@Log - \(type)이 DI 컨테이너에 등록되지 않음.")
+        }
+        return object
+    }
+
+}
