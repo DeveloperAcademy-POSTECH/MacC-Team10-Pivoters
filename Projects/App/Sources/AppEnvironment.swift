9fc0fee3009909a62a193ccf66ee195fbeb7e673
sozohoy <sozohoy@gmail.com>
2024-01-22 19:35:54 +0900

    add: 의존성 주입을 위한 파일 추가(#129)

---
+++ Projects/App/Sources/AppEnvironment.swift
@@ -0,0 +1,9 @@
+//
+//  AppEnvironment.swift
+//  App
+//
+//  Created by 한지석 on 1/22/24.
+//  Copyright © 2024 com.pivoters. All rights reserved.
+//
+
+import Foundation
---
+++ Projects/Core/Sources/DIContainer/DIContainer.swift
@@ -0,0 +1,9 @@
+//
+//  DIContainer.swift
+//  Core
+//
+//  Created by 한지석 on 1/22/24.
+//  Copyright © 2024 com.pivoters. All rights reserved.
+//
+
+import Foundation
