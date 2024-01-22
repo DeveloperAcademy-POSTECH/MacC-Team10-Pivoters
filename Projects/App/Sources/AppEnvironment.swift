852dc469bf8bbdc55f6f67358857b8f6b1587127
sozohoy <sozohoy@gmail.com>
2024-01-22 19:39:21 +0900

    feat: App 모듈에서 의존성 주입(#129)

--- Projects/App/Sources/AppEnvironment.swift
+++ Projects/App/Sources/AppEnvironment.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+import SwiftData
+
+import Core
+import Data
+import Domain
+
+struct AppEnvironment {
+
+    let container = DIContainer.shared
+    let modelContext: ModelContext
+
+    init(modelContext: ModelContext) {
+        self.modelContext = modelContext
+        dependencyInjection()
+    }
+
+    func dependencyInjection() {
+        registerRepositories()
+        registerUseCases()
+    }
+
+    func registerUseCases() {
+        // MARK: - Team UseCases
+        container.register(type: FetchTeamUseCase.self, DefaultFetchTeamUseCase())
+        container.register(type: SaveTeamUseCase.self, DefaultSaveTeamUseCase())
+
+        // MARK: - Lineup UseCases
+        container.register(type: FetchLineupUseCase.self, DefaultFetchLineupUseCase())
+        container.register(type: UpdateLineupUseCase.self, DefaultUpdateLineupUseCase())
+    }
+
+    func registerRepositories() {
+        // MARK: - Team Repository
+        container.register(type: TeamSchemaRepositoryInterface.self,
+                           TeamSchemaRepository(modelContext: modelContext))
+
+        // MARK: - Lineup Repository
+        container.register(type: LineupSchemaRepositoryInterface.self,
+                           LineupSchemaRepository(modelContext: modelContext))
+    }
+
+}
